7076a92e22f4230f5d1a3898d0f38ba73031e553
Valerio Santinelli <santinelli@altralogica.it>
2017-04-24 12:33:36 +0200

    More work on the ShareDB adapter

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,6 +62,7 @@ module.exports = function (grunt) {
           "lib/annotation-list.js",
           "lib/cursor.js",
           "lib/firebase-adapter.js",
+          "lib/sharedb-adapter.js",
           "lib/rich-text-toolbar.js",
           "lib/wrapped-operation.js",
           "lib/undo-manager.js",
